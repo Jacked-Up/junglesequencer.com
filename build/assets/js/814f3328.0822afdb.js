0d4405cf6af31ce31334c1baf7ba3057084274c1
Jack Randolph <jr@jackedupsoftware.com>
2023-12-15 15:46:43 -0500

    Branch node documentation built

---
+++ build/assets/js/814f3328.0822afdb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkjungle_website=self.webpackChunkjungle_website||[]).push([[2535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/blog/welcome","unlisted":false}]}')}}]);
