17cae97353a181ad03b56b0de489a69e17df211f
Jack Randolph <jr@jackedupsoftware.com>
2023-12-22 18:16:50 -0500

    Site built with both generic and event node documentation

--- build/assets/js/c4f5d8e4.68252918.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkjungle_website=self.webpackChunkjungle_website||[]).push([[195],{2841:(e,i,t)=>{t.r(i),t.d(i,{default:()=>l});var n=t(2263),s=t(5924),a=t(5893);function l(){const{siteConfig:e}=(0,n.Z)();return(0,a.jsx)(s.Z,{title:"Home",description:"Description will go into a meta tag in <head />",children:(0,a.jsx)("main",{})})}}}]);
