1fd7e7fd79fbd3427940011a370398ebf8b2668a
JackaPacka <jr@jackedupsoftware.com>
2024-01-12 09:05:45 -0500

    Testing thumbnails...

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,17 @@ export default function Home() {
       title={ `Home` }
       description="A conditional sequencing framework tool for Unity.">
       <main>
-
+          <div className="image-container">
+              <div className="row top-row">
+                  <img src="../../static/img/jungle-thumbnail-1.jpg" alt="Image 1" />
+                  <img src="../../static/img/jungle-thumbnail-2.jpg" alt="Image 2" />
+                  <img src="../../static/img/jungle-thumbnail-3.jpg" alt="Image 3" />
+              </div>
+              <div className="row bottom-row">
+                  <img src="../../static/img/jungle-thumbnail-4.jpg" alt="Image 4" />
+                  <img src="../../static/img/jungle-thumbnail-5.jpg" alt="Image 5" />
+              </div>
+          </div>
       </main>
     </Layout>
   );
