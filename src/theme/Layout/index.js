daf1d82ce17b49296a24704061e23bbc90d5427c
JackaPacka <jr@jackedupsoftware.com>
2024-05-04 15:58:56 -0400

    Update index.js

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Layout from '@theme-original/Layout';
 import CookieConsent from "../../components/CookieConsent";
-import BrowserOnly from "@docusaurus/BrowserOnly";
 
 function LayoutWrapper(props) {
     return (
-        <BrowserOnly>
-            <CookieConsent />
+        <>
+
             <Layout {...props} />
-        </BrowserOnly>
+        </>
     );
 }
 
