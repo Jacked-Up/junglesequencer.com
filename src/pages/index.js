c44aed81833ea465a71b2f262dfebc19659be439
JackaPacka <jr@jackedupsoftware.com>
2024-05-05 17:03:02 -0400

    Feature cards on landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import '../css/index.css';
 import Layout from '@theme/Layout';
 
-import image2 from "./img/jungle-thumbnail-2.png";
-import image3 from "./img/jungle-thumbnail-3.png";
-import image4 from "./img/jungle-thumbnail-4.png";
-
 import unityLogoBlack from "../../static/img/brands/unity/unity-logo-black.png";
-import unityLogoWhite from "../../static/img/brands/unity/unity-logo-white.png";
+//import unityLogoWhite from "../../static/img/brands/unity/unity-logo-white.png";
+
+import feature1 from "../../static/img/feature-cards/feature-card-1.png";
+import feature2 from "../../static/img/feature-cards/feature-card-2.png";
+import feature3 from "../../static/img/feature-cards/feature-card-3.png";
+import feature4 from "../../static/img/feature-cards/feature-card-4.png";
+import feature5 from "../../static/img/feature-cards/feature-card-5.png";
+import feature6 from "../../static/img/feature-cards/feature-card-6.png";
 
 function IndexPage() {
     return (
@@ -32,6 +35,86 @@ function IndexPage() {
             <div className="features-section">
                 <div className="container margin-vert--xl">
 
+                    <div className="features-section-cards">
+
+                        <div className="features-section-card">
+                            <div className="text">
+                                <h2>Creating and Editing Sequences</h2>
+                                <p>
+                                    Drag and drop nodes from the node explorer into the graph view to form a sequence,
+                                    and edit node properties using the inspector.
+                                </p>
+                            </div>
+                            <div className="image">
+                                <img src={feature1} alt="Feature 1"/>
+                            </div>
+                        </div>
+
+                        <div className="features-section-card reverse">
+                            <div className="text">
+                                <h2>Create Nodes with C#</h2>
+                                <p>
+                                    Create performant custom nodes using C# scripts. Nodes are automatically generated
+                                    from the scripts you write.
+                                </p>
+                            </div>
+                            <div className="image">
+                                <img src={feature2} alt="Feature 2"/>
+                            </div>
+                        </div>
+
+                        <div className="features-section-card">
+                            <div className="text">
+                                <h2>Custom Node Inspectors</h2>
+                                <p>
+                                    Design custom inspectors for your nodes to make them more user-friendly and provide
+                                    additional functionality.
+                                </p>
+                            </div>
+                            <div className="image">
+                                <img src={feature3} alt="Feature 3"/>
+                            </div>
+                        </div>
+
+                        <div className="features-section-card reverse">
+                            <div className="text">
+                                <h2>Editor Sticky Notes</h2>
+                                <p>
+                                    Add sticky notes in your trees to document ideas and provide context.
+                                </p>
+                            </div>
+                            <div className="image">
+                                <img src={feature4} alt="Feature 4"/>
+                            </div>
+                        </div>
+
+                        <div className="features-section-card">
+                            <div className="text">
+                                <h2>Editor Gizmos Support</h2>
+                                <p>
+                                    Utilize Unity's gizmos system to provide visual feedback in the editor for your
+                                    nodes.
+                                </p>
+                            </div>
+                            <div className="image">
+                                <img src={feature5} alt="Feature 5"/>
+                            </div>
+                        </div>
+
+                        <div className="features-section-card reverse">
+                            <div className="text">
+                                <h2>Validator & Error Handler</h2>
+                                <p>
+                                    Implement validation logic in your nodes to catch issues before they occur.
+                                </p>
+                            </div>
+                            <div className="image">
+                                <img src={feature6} alt="Feature 6"/>
+                            </div>
+                        </div>
+
+                    </div>
+
                     <div className="features-section-unity">
 
                         <div>
@@ -42,7 +125,7 @@ function IndexPage() {
                                 Supported on Unity versions <span className="text-glow"><strong>2021.3 +</strong></span>
                             </p>
                         </div>
-                        <img src={unityLogoBlack} alt="Unity Logo (black)" />
+                        <img src={unityLogoBlack} alt="Unity Logo (black)"/>
 
                     </div>
 
@@ -59,7 +142,7 @@ function IndexPage() {
                     <br/>
 
                     <div>
-                    <h2>Discord Server</h2>
+                        <h2>Discord Server</h2>
                         <p>
                             Chat with the community, ask questions, and get help with your projects.
                         </p>
