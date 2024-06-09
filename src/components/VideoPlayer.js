a76a6143c41248500dbcabcf425b23ca3649859f
JackaPacka <jr@jackedupsoftware.com>
2024-06-09 17:34:37 -0400

    Video embeds for graph, inspector, explorer, and sticky note guides

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
+import '../css/VideoPlayer.css';
 
-const VideoPlayer = ({ url }) => {
-    return <ReactPlayer url={url} controls={true} />;
+const VideoPlayer = ({ videoUrl, title, description }) => {
+    return (
+        <div className="video-player">
+            <div className="video-container">
+                <div className="player-wrapper">
+                    <ReactPlayer
+                        url={videoUrl}
+                        controls={true}
+                        width="100%"
+                        height="100%"
+                        className="react-player"
+                    />
+                </div>
+            </div>
+            <div className="text-container">
+                <h2>{title}</h2>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
 };
 
 export default VideoPlayer;
