dd9f61bdc03b06ca9444375761bf5fc99cc4b6b2
Jack Randolph <jr@jackedupsoftware.com>
2025-01-14 16:54:46 -0500

    Implemented component for adding a border around sections

---
+++ src/components/BorderedSection.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import "../css/BorderedSection.css";
+
+export default function BorderedSection({ children }) {
+    return <div className={'borderedSection'}>{children}</div>;
+}
