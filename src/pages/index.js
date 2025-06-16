5a73fe89bb71b3632fdd628ff3e247d55b2c8db8
JackaPacka <jr@jackedupsoftware.com>
2025-06-16 14:38:28 -0400

    More CDN implementation on the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,11 @@ import React, { useEffect } from "react";
 import '../css/index.css';
 import Layout from '@theme/Layout';
 
+// URL config
+const OVERVIEW_VIDEO_URL = "https://www.youtube.com/embed/mX08Ofr6Fcc?enablejsapi=1";
+const DISCORD_SERVER_URL = "https://discord.gg/vJ4AXsExfE";
+const GITHUB_DISCUSSIONS_URL = "https://github.com/Jacked-Up/junglesequencer.com/discussions";
+
 function IndexPage() {
     return (
         <Layout>
@@ -17,7 +22,7 @@ function IndexPage() {
                     </a>
                 </div>
             </div>
-
+            
             <div className="features-section">
                 <div className="container margin-vert--xl">
                     <div className="features-video">
@@ -26,7 +31,9 @@ function IndexPage() {
                             Watch this video to see Jungle Sequencer in action and learn how it can enhance your development workflow.
                         </p>
                         <div className="video-wrapper">
-                            <iframe width="700" height="394" src="https://www.youtube.com/embed/mX08Ofr6Fcc?enablejsapi=1" frameBorder="0" allowFullScreen></iframe>
+                            <iframe width="700" height="394" src={OVERVIEW_VIDEO_URL} frameBorder="0" allowFullScreen>
+                                Jungle Sequencer Overview YouTube video.
+                            </iframe>
                         </div>
                     </div>
                     
@@ -122,33 +129,21 @@ function IndexPage() {
                 <div className="container margin-vert--xl">
                     <h1>Community</h1>
                     <p>Connect with other developers, share your projects, and get help with Jungle Sequencer.</p>
-
                     <br/>
-
                     <div>
                         <h2>Discord Server</h2>
                         <p>Chat with the community, ask questions, and get help with your projects.</p>
-                        <a href="https://discord.gg/vJ4AXsExfE" target="_blank" rel="noopener noreferrer"
-                           className="discord-button">
-                            <img
-                                src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/653714c1f22aef3b6921d63d_636e0a6ca814282eca7172c6_icon_clyde_white_RGB.svg"
-                                alt="Discord" height="20"/>
+                        <a href={DISCORD_SERVER_URL} target="_blank" rel="noopener noreferrer" className="discord-button">
+                            <img src="https://cdn.junglesequencer.com/homepage/discord-logo.png" alt="Discord" height="20"/>
                             <strong>Join Server</strong>
                         </a>
                     </div>
-
                     <br/>
-
                     <div>
                         <h2>GitHub Discussions</h2>
-                        <p>
-                            Join the discussions on GitHub to explore more topics and engage with the community.
-                        </p>
-                        <a href="https://github.com/Jacked-Up/junglesequencer.com/discussions" target="_blank"
-                           rel="noopener noreferrer" className="github-button">
-                            <img
-                                src="https://iconmonstr.com/wp-content/g/gd/makefg.php?i=../releases/preview/2012/png/iconmonstr-github-1.png&r=255&g=255&b=255"
-                                alt="GitHub" height="24"/>
+                        <p>Join the discussions on GitHub to explore more topics and engage with the community.</p>
+                        <a href={GITHUB_DISCUSSIONS_URL} target="_blank" rel="noopener noreferrer" className="github-button">
+                            <img src="https://cdn.junglesequencer.com/homepage/github-logo.png" alt="GitHub" height="24"/>
                             <strong>Join Discussions</strong>
                         </a>
                     </div>
