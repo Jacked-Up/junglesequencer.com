fb4598e5d09290197573263327d01490fa0add06
JackaPacka <jr@jackedupsoftware.com>
2025-06-16 17:25:27 -0400

    Default site theme is now dark (unless overridden by browser settings)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,12 @@ const config = {
   favicon: 'img/favicon.ico',
   url: 'https://junglesequencer.com',
   baseUrl: '/',
-
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
   },
-
   presets: [
     [
       'classic',
@@ -41,104 +38,104 @@ const config = {
       }),
     ],
   ],
-  
-  themeConfig:
-    ({
-      image: 'img/docusaurus-social-card.jpg',
-      navbar: {
-        title: 'Jungle',
-        logo: {
-          alt: 'Jungle Sequencer Logo',
-          src: 'img/logo.png',
+  themeConfig: {
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: false,
+      respectPrefersColorScheme: true,
+    },
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Jungle',
+      logo: {
+        alt: 'Jungle Sequencer Logo',
+        src: 'img/logo.png',
+      },
+      items: [
+        {
+          type: 'docsVersionDropdown',
+          position: 'right',
+          dropdownActiveClassDisabled: true,
         },
-        items: [
-          {
-            type: 'docsVersionDropdown',
-            position: 'right',
-            dropdownActiveClassDisabled: true,
-          },
-          {
-            type: 'docSidebar',
-            sidebarId: 'documentationSidebar',
-            position: 'left',
-            label: 'Documentation',
-          },
-          {
-            type: 'docSidebar',
-            sidebarId: 'guidesSidebar',
-            position: 'left',
-            label: 'Guides',
-          },
-          {
-            to: '/blog',
-            label: 'Blog/Changelog',
-            position: 'left'
-          },
-          {
-            href: 'https://assetstore.unity.com/packages/slug/258407',
-            label: 'Buy Jungle',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        links: [
-          {
-            title: 'Links',
-            items: [
-              {
-                label: 'Unity Asset Store',
-                href: 'https://assetstore.unity.com/packages/slug/258407',
-              },
-              {
-                label: 'Jacked Up Software',
-                href: 'https://jackedupsoftware.com',
-              },
-            ],
-          },
-          {
-            title: 'Socials',
-            items: [
-              {
-                label: 'Discord Server',
-                href: 'https://discord.gg/vJ4AXsExfE',
-              },
-              {
-                label: 'GitHub Discussions',
-                href: 'https://github.com/Jacked-Up/junglesequencer.com/discussions',
-              },
-              {
-                label: 'Trello Board',
-                href: 'https://trello.com/b/hSJvNBCh/jungle-sequencer',
-              }
-            ],
-          },
-          {
-            title: 'Legal',
-            items: [
-              {
-                label: 'Terms & Conditions',
-                href: 'https://jackedupsoftware.com/legal/terms-and-conditions',
-              },
-              {
-                label: 'Privacy Policy',
-                href: 'https://jackedupsoftware.com/legal/privacy-policy',
-              },
-            ],
-          },
-        ],
-        copyright: `<br/> © 2025 Jacked Up Software`,
-      },
-      prism: {
-        theme: prismThemes.vsLight,
-        darkTheme: prismThemes.vsDark,
-        additionalLanguages: ['csharp']
-      },
-    }),
-  
-    plugins: [
-      '@easyops-cn/docusaurus-search-local'
-    ],
+        {
+          type: 'docSidebar',
+          sidebarId: 'documentationSidebar',
+          position: 'left',
+          label: 'Documentation',
+        },
+        {
+          type: 'docSidebar',
+          sidebarId: 'guidesSidebar',
+          position: 'left',
+          label: 'Guides',
+        },
+        {
+          to: '/blog',
+          label: 'Blog/Changelog',
+          position: 'left',
+        },
+        {
+          href: 'https://assetstore.unity.com/packages/slug/258407',
+          label: 'Buy Jungle',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      links: [
+        {
+          title: 'Links',
+          items: [
+            {
+              label: 'Unity Asset Store',
+              href: 'https://assetstore.unity.com/packages/slug/258407',
+            },
+            {
+              label: 'Jacked Up Software',
+              href: 'https://jackedupsoftware.com',
+            },
+          ],
+        },
+        {
+          title: 'Socials',
+          items: [
+            {
+              label: 'Discord Server',
+              href: 'https://discord.gg/vJ4AXsExfE',
+            },
+            {
+              label: 'GitHub Discussions',
+              href: 'https://github.com/Jacked-Up/junglesequencer.com/discussions',
+            },
+            {
+              label: 'Trello Board',
+              href: 'https://trello.com/b/hSJvNBCh/jungle-sequencer',
+            },
+          ],
+        },
+        {
+          title: 'Legal',
+          items: [
+            {
+              label: 'Terms & Conditions',
+              href: 'https://jackedupsoftware.com/legal/terms-and-conditions',
+            },
+            {
+              label: 'Privacy Policy',
+              href: 'https://jackedupsoftware.com/legal/privacy-policy',
+            },
+          ],
+        },
+      ],
+      copyright: `<br/> © 2025 Jacked Up Software`,
+    },
+    prism: {
+      theme: prismThemes.vsLight,
+      darkTheme: prismThemes.vsDark,
+      additionalLanguages: ['csharp'],
+    },
+  },
+  plugins: ['@easyops-cn/docusaurus-search-local'],
 };
 
-export default config;
+module.exports = config;
