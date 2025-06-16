032e8374854ebb925a510ec832de5173eeeef682
JackaPacka <jr@jackedupsoftware.com>
2025-06-16 15:32:49 -0400

    v1.1.0 documentation and guides are now completely using new CDN

--- sidebars.js
+++ sidebars.js
@@ -12,7 +12,7 @@ const sidebars = {
             },
             collapsed: true,
             items: [
-                'documentation/jungle-tree/jungle-manifest',
+                'documentation/jungle-tree/jungle-manifest'
             ],
         },
         {
@@ -79,33 +79,30 @@ const sidebars = {
             items: [
                 'documentation/editor-only/jungle-editor-utils',
                 'documentation/editor-only/jungle-validator',
-                'documentation/editor-only/jungle-preferences',
+                'documentation/editor-only/jungle-preferences'
             ],
         },
     ],
     guidesSidebar: [
         {
             type: 'category',
-            label: 'Creating Node Scripts',
+            label: 'Creating Nodes',
             link: {
-                type: 'generated-index',
+                type: 'doc',
+                id: 'guides/creating-nodes/node-fabricator',
             },
             collapsed: true,
             items: [
-                'guides/creating-node-scripts/using-the-node-fabricator',
-                'guides/creating-node-scripts/creating-branch-nodes',
-                'guides/creating-node-scripts/creating-io-nodes',
-                'guides/creating-node-scripts/creating-identity-nodes',
-                'guides/creating-node-scripts/creating-event-nodes'
+                'guides/creating-nodes/branch-node',
+                'guides/creating-nodes/io-node',
+                'guides/creating-nodes/identity-node',
+                'guides/creating-nodes/event-node'
             ],
         },
-        'guides/deleting-node-scripts',
+        'guides/deleting-nodes',
         {
             type: 'category',
             label: 'Jungle Editor',
-            link: {
-                type: 'generated-index',
-            },
             collapsed: true,
             items: [
                 'guides/jungle-editor/graph-panel',
@@ -116,21 +113,22 @@ const sidebars = {
         },
         {
             type: 'category',
-            label: 'Node Context Features',
-            link: {
-                type: 'generated-index',
-            },
+            label: 'Node Features',
             collapsed: true,
             items: [
-                'guides/node-context-features/details-box',
-                'guides/node-context-features/progress-bar',
-                'guides/node-context-features/control-panel',
-                'guides/node-context-features/gizmos'
+                'guides/node-features/details-box',
+                'guides/node-features/progress-bar',
+                'guides/node-features/control-panel',
+                'guides/node-features/gizmos',
+                'guides/node-features/custom-inspector'
             ],
         },
-        'guides/custom-node-inspectors',
         'guides/revert-actions',
-        'guides/jungle-validator'
+        'guides/over-time-helper',
+        
+        
+        'guides/jungle-validator',
+        'guides/jungle-preferences'
     ],
 };
 
