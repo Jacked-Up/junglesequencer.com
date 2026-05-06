9f1fe13ce8706fd9f172cf75b04dcc81e4f7f6dc
JackaPacka <jr@jackedupsoftware.com>
2026-05-06 10:07:24 -0400

    Bunch of updates and such

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -4,17 +4,19 @@ import Layout from '@theme-original/Layout';
 import CookieConsent from "@site/src/components/CookieConsent";
 import SocialsPrompt from "@site/src/components/SocialsPrompt";
 
-export default function LayoutWrapper(props) {
+function LayoutWrapper(props) {
     return (
         <>
             { /* Cookies prompt */ }
             <CookieConsent />
             
             { /* Socials prompt */ }
-             <SocialsPrompt />
+            <SocialsPrompt />
             
             { /* Main Docusaurus layout */ }
             <Layout {...props} />
         </>
     );
 }
+
+export default LayoutWrapper;
